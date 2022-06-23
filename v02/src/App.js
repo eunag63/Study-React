4125d08956c0e7c7ffde9351d478968d6d3bd73e
eunag63 <eunag39@gmail.com>
2022-06-23 21:50:19 +0900

    🎨 프로젝트 기초공사 2

--- v02/src/App.js
+++ v02/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import React, { useState, useRef, useReducer } from 'react';
 import './App.css';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
@@ -11,25 +11,78 @@ import Test from './com/Test';
 import Button from './com/Button';
 import Header from './com/Header'
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'init': {
+      return action.data;
+    }
+    case 'crate': {
+      const newitem = {
+        ...action.data
+      };
+      newState = [newitem, ...state];
+      break;
+    }
+    case 'remove': {
+      newState = state.filter((it) => it.id != action.targetId);
+      break;
+    }
+    case 'edit': {
+      newState = state.map((it) => it.id === action.data.id? {...action.data}: it)
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+}
+export const DContext = React.createContext();
+export const DisplayContext = React.createContext();
 function App() {
+  const [data, dispatch] = useReducer(reducer, [])
+  const dataId = useRef(0);
+  const onCreate = (date, content, emotion) => {
+    dispatch({type: "crate", data: {
+      id: dataId.curret,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }});
+    dataId.current += 1;
+  }
+  const onRemove = (id) => {
+    dispatch({type: "remove", id});
+  }
+  const onEdit = (id, date, content,emotion) => {
+    dispatch({
+      type: "edit",
+      data: {
+        id: id,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+  }
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header text={"앱이라고"} left={<Button text={'왼쪽버튼'} onClick={()=>alert("왼쪽클릭")}></Button>} right={<Button text={'오른쪽버튼'} onClick={()=>alert("오른쪽클릭")}></Button>}></Header>
-        <Button text={'버튼'} onClick={()=>alert("버튼클릭")} type={"positive"}></Button>
-        <Button text={'버튼'} onClick={()=>alert("버튼클릭")} type={"negative"}></Button>
-        <Button text={'버튼'} onClick={()=>alert("버튼클릭")} type={""}></Button>        
-        <Routes>
-          <Route path='/' element={<H></H>}></Route>
-          <Route path='/n' element={<N></N>}></Route>
-          <Route path='/e' element={<E></E>}></Route>
-          <Route path='/d/:id' element={<D></D>}></Route>
-        </Routes>
-        <Test></Test>
-        <h1>일기장</h1>
-        
-      </div>
-    </BrowserRouter>
+    <DContext.Provider value={data}>
+      <DisplayContext.Provider value={{onCreate, onEdit, onRemove}}>
+      <BrowserRouter>
+        <div className="App">    
+          <Routes>
+            <Route path='/' element={<H></H>}></Route>
+            <Route path='/n' element={<N></N>}></Route>
+            <Route path='/e' element={<E></E>}></Route>
+            <Route path='/d/:id' element={<D></D>}></Route>
+          </Routes>
+          <Test></Test>
+          <h1>일기장</h1>
+          
+        </div>
+      </BrowserRouter>
+      </DisplayContext.Provider>
+    </DContext.Provider>
     
   );
 }
