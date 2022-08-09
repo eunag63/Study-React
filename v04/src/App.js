0d73c9ca66db5306e18fddf5df1ad7738e8e13bd
Euna Kim <kibm63@famoz.co.kr>
2022-08-09 16:34:18 +0900

    📝 ~6.4) useEffect

--- v04/src/App.js
+++ v04/src/App.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 import Button from "./Button";
 import styles from './App.module.css';
 
 function App() {
+  useEffect(() => {
+    console.log("Call the one!");
+  }, []);
+
+
+  function Hello() {
+    function byFn() {
+      console.log("bye");
+    }
+    function hiFn() {
+      console.log("hi");
+      return byFn;
+    }
+    useEffect(hiFn, []);
+    return <hi>Hello</hi>
+  }
+
+
   return (
     <div>
       <h1 className={styles.title}>Welcome Back!</h1>
